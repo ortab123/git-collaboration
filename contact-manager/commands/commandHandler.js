ece18559e0ff3b89e02051ac16084bf09f76b3e4
Zinger Marat <zingermarat@gmail.com>
2025-07-14 17:00:53 +0300

    implement command handler functions for add, list, search, delete, and help commands

---
+++ contact-manager/commands/commandHandler.js
@@ -0,0 +1,15 @@
+
+const commandAdd = () => {}
+const commandList = () => {}
+const commandSearch = () => {}
+const commandDelete = () => {}
+const commandHelp = () => {}
+
+
+module.export ={
+    commandAdd,
+    commandList,
+    commandSearch,
+    commandDelete,
+    commandHelp
+}
